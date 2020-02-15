773624c527a4fdc8cf943155c69146f69c52347e
yamini-ban <yaminibansal7@gmail.com>
2020-02-16 03:13:35 +0530

    occurrence method added

--- src/main/scala/edu/knoldus/etl/SourceIsTheFile.scala
+++ src/main/scala/edu/knoldus/etl/SourceIsTheFile.scala
@@ -21,25 +21,50 @@ class SourceIsTheFile(val nameOfTheFileToManipulate: String) extends ManipulateF
       val _ = new File(directoryName).mkdir
       val contentOfFetchedFile = Source.fromFile(fetchFile)
       val linesInFile = contentOfFetchedFile.getLines
-      val printWriter = new PrintWriter(new File(s"${directoryName}/O-${fetchFile.getName}"))
+      val printWriter = new PrintWriter(new File(s"$directoryName/O-${fetchFile.getName}"))
       linesInFile.foreach(line => printWriter.write(line.toUpperCase()+"\n"))
       printWriter.close()
-      contentOfFetchedFile.close()
+      contentOfFetchedFile.close
     }
     else throw new CustomException("File does not exist.")
   }
 
-  override def OccurrenceOfEveryWordInTheData() {
+  override def occurrenceOfEveryWordInTheData() {
     val fetchFile = new File(nameOfTheFileToManipulate)
     if (fetchFile.exists && fetchFile.isFile) {
       val directoryName = "Count-Result"
       val _ = new File(directoryName).mkdir
       val contentOfFetchedFile = Source.fromFile(fetchFile)
       val linesInFile = contentOfFetchedFile.getLines
+      val printWriter = new PrintWriter(new File(s"${directoryName}/O-${fetchFile.getName}"))
+      linesInFile.foreach(line => {
+        line.split(" ").foreach(word => printWriter.write(s"$word -> ${line.split(word).length - 1}\n"))
+      })
 
+      /*val _ = linesInFile.foldLeft(Map[String, Int])((_, line) => {
+        line.split(" ").foldLeft(Map[String, Int])((result, word) => {
+          result + (word -> getOccurrence(line, word))
+        })
+      })*/
+
+      val map = Map.empty[String, Int]
+      for {
+        line <- linesInFile
+        word <- line
+        map :+ (word -> getOccurrence(line, word))
+      } yield map
+
+      printWriter.close()
+      contentOfFetchedFile.close
     }
-    else throw new CustomException("File does not exist.")
+//    else throw new CustomException("File does not exist.")
 
+  }
+
+  private def getOccurrence(line: String, occurrenceOfWord: String): Int = {
+    if(line.contains(occurrenceOfWord)) {
+      line.count(o)
+    }
   }
 
 }
